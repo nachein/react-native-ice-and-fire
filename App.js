b4dc5c2e824469f0511b165fbf1d6659083882c7
nachein <ignaciocasal@gmail.com>
2017-09-05 18:00:46 +0200

    Added basic navigation (wip)

--- App.js
+++ App.js
@@ -1,17 +1,6 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
-}
+import { StyleSheet, Text, View, Button, FlatList, ActivityIndicator } from 'react-native';
+import { StackNavigator } from 'react-navigation';
 
 const styles = StyleSheet.create({
   container: {
@@ -20,4 +9,135 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+});
+
+class HomeScreen extends React.Component {
+	static navigationOptions = {
+		title: 'Home'	
+	};
+	render() {
+		const { navigate } = this.props.navigation;
+		return (
+			<View style={{flex: 1}}>
+				<View style={{flex: 1}}>
+					<Text>Hello, Navigation</Text>
+				</View>
+				<View style={{flex: 3}}>
+					<Button
+						title='Characters'
+						onPress={()=>
+							navigate('Characters')
+						}
+					/>
+				</View>
+				<View style={{flex: 3}}>
+					<Button
+						title='Houses'
+						onPress={()=>
+							navigate('Houses')
+						}
+					/>
+				</View>
+				<View style={{flex: 2}}>
+					<Button
+						title='Books'
+						onPress={()=>
+							navigate('Books')
+						}
+					/>
+				</View>
+			</View>
+		);
+	}
+}
+
+class CharactersScreen extends React.Component {
+	static navigationOptions = {
+		title: 'Characters'
+	};
+	constructor(props){
+		super(props);
+		this.state = {
+			isLoading: true,
+			page: 1
+		};
+	}
+	componentDidMount(){
+		this.fetchCharacters();
+	}
+	fetchCharacters(){
+		return fetch(`https://www.anapioficeandfire.com/api/characters?page=${this.state.page}`)
+			.then((response) => response.json())
+			.then((json) => {
+				//let ds = new ListView.DataSource({rowHasChanged: (r1,r2) => r1 !== r2});
+				this.setState({
+					isLoading: false,
+					dataSource: json//ds.cloneWithRows(json)
+				});
+			})
+			.catch((error) => {
+				console.error(error);
+			});
+	}
+	render() {
+		if(this.state.isLoading) {
+			return (
+				<View style={{flex: 1, paddingTop: 20}}>
+					<ActivityIndicator />
+				</View>
+			);
+		}
+ 
+		return (
+			<View style={{flex: 1, paddingTop: 20}}>
+				<View style={{flex: 1}}>
+					<Text> All </Text>
+				</View>
+				<View style={{flex: 5}}>
+					<CharacterList data={this.state.dataSource} />
+				</View>
+			</View>
+		);
+	}
+}
+
+class CharacterList extends React.Component {
+	render() {
+		const { data } = this.props;
+		return (
+			<FlatList
+				data={data}
+				renderItem={this.renderItem}
+			/>
+		);
+	}
+
+	renderItem(itemData){
+		return <View><Text>lala</Text></View>;
+	}
+}
+
+class HousesScreen extends React.Component {
+	static navigationOptions = {
+		title: 'Houses'
+	};
+	render() {
+		return <Text>GOT Houses</Text>;
+	}
+}
+
+class BooksScreen extends React.Component {
+	static navigationOptions = {
+		title: 'Books'
+	};
+	render() {
+		return <Text>Books</Text>
+	}
+}
+
+export default App = StackNavigator({
+  Home: { screen: HomeScreen },
+	Characters: { screen: CharactersScreen },
+	Houses: { screen: HousesScreen },
+	Books: { screen: BooksScreen }
 });
